9f5f920725bb33e42c25efd19fc92a3220f65ab6
Alenxandre silva <xvalexandrexv@gmail.com>
2024-06-17 12:55:42 +0100

    first commit

---
+++ src/main/java/com/example/MongoDb_SB/MongoDbSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.MongoDb_SB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MongoDb_SB/MongoDbSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MongoDb_SB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
